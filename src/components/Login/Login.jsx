08a1b8f2d8764732997813c6729b76da91212b82
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook.local>
2019-09-27 15:11:26 -0400

    add login/register frontend

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,58 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { connect } from "react-redux";
 
 class Login extends Component {
   constructor() {
     super();
 
     this.state = {
-      email: "",
+      username: "",
       password: ""
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.handleInput = this.handleInput.bind(this);
     this.login = this.login.bind(this);
   }
 
-  handleChange() {}
+  handleInput(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
 
-  login() {}
+  async login() {
+    const { username, password } = this.state;
+    axios.post("/api/login", { username, password }).then(res => {
+      console.log(res);
+    });
+  }
 
   render() {
-    return <div className="Login">Login</div>;
+    return (
+      <div>
+        <h2>Welcome to hooplead</h2>
+        <h3>Please Sign-In</h3>
+        <form>
+          <p>Username</p>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={this.handleInput}
+          />
+          <p>Password</p>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={this.handleInput}
+          />
+        </form>
+        <button onClick={this.login}>Sign-In</button>
+        <p>
+          New user? Register <Link to="/register">here</Link>
+        </p>
+      </div>
+    );
   }
 }
 
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { async } from "q";
 
 class Register extends Component {
   constructor() {
@@ -9,16 +11,62 @@ class Register extends Component {
       email: "",
       password: ""
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.handleInput = this.handleInput.bind(this);
     this.register = this.register.bind(this);
   }
 
-  handleChange() {}
+  handleInput(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
 
-  register() {}
+  register() {
+    const { username, email, password } = this.state;
+    axios
+      .post("/api/register", { username, email, password })
+      .then(async res => {
+        console.log(res);
+        if (res.status === 200) {
+          let resWithCoachId = await axios.get();
+        }
+      })
+      .catch(err => {
+        alert(
+          "Registration failed, please try a different username and password."
+        );
+        window.location.reload();
+      });
+  }
 
   render() {
-    return <div className="Register">Register</div>;
+    return (
+      <div>
+        <h3>Register below to start tracking your teams!</h3>
+        <form>
+          <p>Username</p>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={this.handleInput}
+          />
+          <p>Email</p>
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            onChange={this.handleInput}
+          />
+          <p>Password</p>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={this.handleInput}
+          />
+        </form>
+        <button onClick={this.register}>Register</button>
+      </div>
+    );
   }
 }
 
