9ecc2eb93898f1683d35a7be80596a045c4c2cdd
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook.local>
2019-09-30 12:28:48 -0400

    bodyparser back

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
+const bodyParser = require("body-parser");
 const massive = require("massive");
 const auth_controller = require("./controllers/auth_controller");
 const team_controller = require("./controllers/team_controller");
 
 const app = express();
-app.use(express.static(`${__dirname}/../build`));
+// app.use(express.static("build"));
+app.use(bodyParser.json());
 
 let { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env;
 
@@ -16,9 +18,9 @@ app.use(
     resave: true,
     saveUninitialized: true,
     cookie: {
-      maxAge: 2592000000
+      maxAge: 259200000
     },
-    expires: 25920000
+    expires: 259200000
   })
 );
 
